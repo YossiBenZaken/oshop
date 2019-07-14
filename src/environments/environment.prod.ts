57ebc2096a8e206a745d06cf2124e7bf42fd81f3
Yossi Ben Zaken <yossibz@gmail.com>
2019-07-14 20:53:07 +0300

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA2RPwly-iFz0ODptfENTaXdXOC3GDBfJU',
+    authDomain: 'oshop-2a15d.firebaseapp.com',
+    databaseURL: 'https://oshop-2a15d.firebaseio.com',
+    projectId: 'oshop-2a15d',
+    storageBucket: '',
+    messagingSenderId: '689996736978',
+    appId: '1:689996736978:web:1268d09613b987a9'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA2RPwly-iFz0ODptfENTaXdXOC3GDBfJU',
+    authDomain: 'oshop-2a15d.firebaseapp.com',
+    databaseURL: 'https://oshop-2a15d.firebaseio.com',
+    projectId: 'oshop-2a15d',
+    storageBucket: '',
+    messagingSenderId: '689996736978',
+    appId: '1:689996736978:web:1268d09613b987a9'
+  }
 };
 
 /*
